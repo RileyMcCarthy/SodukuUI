6c2985e85855d74048d3de30ada439fc53df9546
Riley McCarthy <mccarthr@uoguelph.ca>
2020-01-11 18:57:18 -0500

    javaFX is now setup in the project

--- src/gui/Interface.java
+++ src/gui/Interface.java
@@ -1,4 +1,19 @@
 package gui;
 
-public class Interface {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Interface extends Application {
+    private Stage stage;
+    private Controller controller;
+
+    @Override
+    public void start(Stage theStage) throws Exception {
+    stage = theStage;
+    stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/soduku/Soduku.java
+++ src/soduku/Soduku.java
@@ -10,10 +10,6 @@ package soduku;
  * @author Riley McCarthy
  */
 public class Soduku {
-
-    /**
-     * @param args the command line arguments
-     */
     
     private static int[][] tempboard = { {0,0,0,0,8,0,0,0,0},
                               {8,0,9,0,7,1,0,2,0},
@@ -24,15 +20,11 @@ public class Soduku {
                               {9,0,0,0,0,0,7,0,2},
                               {0,0,8,2,0,5,0,9,0},
                               {1,0,0,0,4,0,3,0,0} };
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public void game() {
         Board board = new Board(tempboard);
         Solver solver = new Solver(board);
-        System.out.println("Soduku Game Generator");
-       // board.printBoard();
         solver.solveBoard();
-        //System.out.println(board.getQuadrent(1,2));
     }
     
 }
