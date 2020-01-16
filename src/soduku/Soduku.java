3f24d685347ce29b1860245118af093c1c37e60a
Riley McCarthy <mccarthr@uoguelph.ca>
2020-01-15 23:53:12 -0500

    Added import capibility, with a csv file containing an empty sudoku game

--- src/soduku/Soduku.java
+++ src/soduku/Soduku.java
@@ -5,6 +5,11 @@
  */
 package soduku;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Riley McCarthy
@@ -13,24 +18,52 @@ public class Soduku {
 
     Solver solver;
 
-    private static int[][] tempboard = { {0,0,0,0,8,0,0,0,0},
-                              {8,0,9,0,7,1,0,2,0},
-                              {4,0,3,5,0,0,0,0,1},
-                              {0,0,0,1,0,0,0,0,7},
-                              {0,0,2,0,3,4,0,8,0},
-                              {7,3,0,0,0,9,0,0,4},
-                              {9,0,0,0,0,0,7,0,2},
-                              {0,0,8,2,0,5,0,9,0},
-                              {1,0,0,0,4,0,3,0,0} };
+   // private static int[][] tempboard = { {0,0,0,0,8,0,0,0,0},
+    //                          {8,0,9,0,7,1,0,2,0},
+    //                       {4,0,3,5,0,0,0,0,1},
+    //                          {0,0,0,1,0,0,0,0,7},
+    //                          {0,0,2,0,3,4,0,8,0},
+    //                          {7,3,0,0,0,9,0,0,4},
+    //                          {9,0,0,0,0,0,7,0,2},
+    //                          {0,0,8,2,0,5,0,9,0},
+    //                          {1,0,0,0,4,0,3,0,0} };
+    private int[][] boardArray;
 
     public Soduku() {
-        Board board = new Board(tempboard);
+        boardArray = importBoard();
+        Board board = new Board(boardArray);
         solver = new Solver(board);
         solver.solveBoard();
     }
 
-    public void importBoard() {
+    public int[][] importBoard() {
+        BufferedReader reader;
+        int[][] tempBoard = new int[9][9];
+        try {
+            reader = new BufferedReader(new FileReader("gameData.txt"));
+            String row;
+            int i = 0;
+            while((row = reader.readLine())!=null) {
+                if (i==9)
+                    return null;
+                String[] temp = row.split(",");
+                int j = 0;
+                for (String str : temp) {
+                    if (j==9)
+                        return null;
+                    tempBoard[i][j] = Integer.parseInt(str);
+                    j++;
+                }
+                i++;
+            }
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tempBoard;
     }
 
     public Cell getNextStep() {
